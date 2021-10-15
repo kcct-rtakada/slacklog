2e69bb2b05b893189cf732c432578564020901f0
KantaTamura <tkanta496@gmail.com>
2021-10-15 22:46:42 +0900

    :sparkles: add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,60 @@
-import * as React from "react"
-import PropTypes from "prop-types"
+import React from "react"
 import { Link } from "gatsby"
+import "../styles/header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">
+        <h1>
+          <span className="sai">sai.ac</span>
+          <span className="slash">/</span>
+          <span className="slack">slacklog</span>
+        </h1>
+      </Link>
+    </header>
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+// import * as React from "react"
+// import PropTypes from "prop-types"
+// import { Link } from "gatsby"
+//
+// const Header = ({ siteTitle }) => (
+//   <header
+//     style={{
+//       background: `rebeccapurple`,
+//       marginBottom: `1.45rem`,
+//     }}
+//   >
+//     <div
+//       style={{
+//         margin: `0 auto`,
+//         maxWidth: 960,
+//         padding: `1.45rem 1.0875rem`,
+//       }}
+//     >
+//       <h1 style={{ margin: 0 }}>
+//         <Link
+//           to="/"
+//           style={{
+//             color: `white`,
+//             textDecoration: `none`,
+//           }}
+//         >
+//           {siteTitle}
+//         </Link>
+//       </h1>
+//     </div>
+//   </header>
+// )
+//
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+//
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
+//
+// export default Header
