a5e1a91436a875b1710453a692ac95856ae7857f
KantaTamura <tkanta496@gmail.com>
2021-10-20 21:29:57 +0900

    :sparkles: add nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
+import "../styles/index.css"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <Layout>
-      <div>Hello world!</div>
+      <div className={"margin-16"}>
+        <h4 className={"channels"}>Channels</h4>
+        <nav className={"nav"}>
+          <Link to={`/channels/general`} className={"nav-items"}>#general</Link>
+          <Link to={'/channels/random'} className={"nav-items"}>#random</Link>
+        </nav>
+      </div>
     </Layout>
   )
 }
