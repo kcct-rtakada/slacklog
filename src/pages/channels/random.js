43940020ff832d4d6b86bcaff3c8e5e80d03e523
KantaTamura <tkanta496@gmail.com>
2021-10-22 22:40:32 +0900

    :sparkles: page template

--- src/pages/channels/random.js
+++ src/pages/channels/random.js
@@ -1,10 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../../components/layout"
 
 export default function random() {
   return (
     <>
-
+      <Layout>
+        <h4>#random</h4>
+        <nav>
+          <Link to={`/`}></Link>
+        </nav>
+      </Layout>
     </>
   )
 }
