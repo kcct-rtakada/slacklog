04262c86d6a94051e5b5481c18cd31066a99cd4f
KantaTamura <tkanta496@gmail.com>
2021-10-27 23:21:17 +0900

    :sparkles: add new for Channels

--- htmlize_slacklog/src/parse_json/channels.rs
+++ htmlize_slacklog/src/parse_json/channels.rs
@@ -1,16 +1,37 @@
 use serde_json::{Result, Value};
 
-pub struct channel {
+#[derive(Debug, PartialOrd, PartialEq)]
+pub struct Channels {
     id: String,
     name: String,
 }
 
-impl channel {
-
+impl Channels {
+    fn new(v: Value) -> Self {
+        Channels {
+            id: v["id"].to_string().chars().filter(|s| !['\"'].contains(&s)).collect(),
+            name: v["name"].to_string().chars().filter(|s| !['\"'].contains(&s)).collect(),
+        }
+    }
 }
 
 #[test]
-fn slacklog_json_parse_test() {
+fn channel_short_log_json_parse_test() {
+    let data = r#"
+{
+    "id": "C01TD4HG17U",
+    "name": "general"
+}
+        "#;
+    let v: Value = serde_json::from_str(data).unwrap();
+    let channel = Channels::new(v);
+
+    let ans: Channels = Channels{id: "C01TD4HG17U".to_string(), name: "general".to_string()};
+    assert_eq!(ans, channel);
+}
+
+#[test]
+fn channel_long_log_json_parse_test() {
     let data = r#"
 {
     "id": "C01TD4HG17U",
@@ -37,6 +58,10 @@ fn slacklog_json_parse_test() {
     }
 }
         "#;
+    let v: Value = serde_json::from_str(data).unwrap();
+    let channel = Channels::new(v);
 
+    let ans: Channels = Channels{id: "C01TD4HG17U".to_string(), name: "general".to_string()};
+    assert_eq!(ans, channel);
 }
 
